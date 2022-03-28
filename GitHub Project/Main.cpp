f0013af17a884b300e4b6853f021e01a1d9f0e57
OverGamerly <Kirill.Drozd15@gmail.com>
2022-03-28 17:51:20 +0300

    Update Main.cpp

---
+++ GitHub Project/Main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int* firstNumb = new int;
+	int* secondNumb = new int;
+
+	cout << "Enter firts numb: ";
+	cin >> *firstNumb;
+
+	cout << "Enter second numb: ";
+	cin >> *secondNumb;
+
+	cout << "Max numb: " << (*firstNumb > *secondNumb ? *firstNumb : *secondNumb);
+
+	return 0;
+}
